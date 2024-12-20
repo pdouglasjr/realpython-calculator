800bd639462d4924513ef976f8e204255e72584e
Phillip Douglas <pdouglasjr@proton.me>
2024-12-19 23:54:52 -0500

    updates

--- calculator.py
+++ calculator.py
@@ -3,8 +3,10 @@ calculator.py
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def substract(first_term, second_term):
-    return first_term - second_term 
+    return first_term - second_term
